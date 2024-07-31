37da7eca6095ceb5f932b58686519d83f3063831
Bobcodiz <mirowe.okinyi22@students.dkut.ac.ke>
2024-07-31 21:20:12 +0300

    All done

--- AddDishes.py
+++ AddDishes.py
@@ -1,4 +1,4 @@
-import UpdateDish
+from UpdateDish import read_price
 
 
 def add_dish(menu):
@@ -12,42 +12,37 @@ def add_dish(menu):
 
     category = input('Enter the category of dish to add: ')
 
+    name = input('Enter the name of the Dessert: ')
+    if check_duplicate(menu, name):
+        print("The Dish Already Exist!")
+        return
+
+    description = input('Enter the description of the dish (Ingredients): ')
+    dish_price = read_price()
+
+    if dish_price is None:
+        print("Returning to main!")
+        return
+
     if category == '1' or category == 'Appetizers':
-        try:
-            name = input('Enter the name of the Appetizer: ')
-            description = input('Enter the description of the dish (Ingredients): ')
-            p = UpdateDish.read_price()
-            menu["Appetizers"].append({"name": name, "description": description, "price": p, "category": "Appetizers"})
-            print("Appetizer added successfully")
-
-        except Exception as e:
-            print("could not add the item to the menu")
-            print(e)
-            return
-
+        menu["Appetizers"].append({"name": name, "description": description, "price": dish_price,
+                                   "category": "Appetizers"})
+        print("Appetizer added successfully")
     elif category == '2' or category == 'Main Courses':
-        try:
-            name = input('Enter the name of the Main Course Dish: ')
-            description = input('Enter the description of the dish  (Ingredients): ')
-            p = UpdateDish.read_price()
-            menu["Main Course"].append(
-                {"name": name, "description": description, "price": p, "category": "Main Course"})
-            print("Main course added successfully")
-        except Exception as e:
-            print("could not add the item to the menu")
-            print(e)
-            return
+        menu["Main Course"].append({"name": name, "description": description, "price": dish_price,
+                                    "category": "Main Course"})
+        print("Main course added successfully")
     elif category == '3' or category == 'Desserts':
-        try:
-            name = input('Enter the name of the Dessert: ')
-            description = input('Enter the description of the dish (Ingredients): ')
-            p = UpdateDish.read_price()
-            menu["Desserts"].append({"name": name, "description": description, "price": p, "category": "Desserts"})
-            print("Dessert added successfully")
-        except Exception as e:
-            print("could not add the dessert")
-            print(e)
-            return
+        menu["Desserts"].append({"name": name, "description": description, "price": dish_price, "category": "Desserts"})
+        print("Desserts added successfully")
     else:
         print("Invalid category")
         return add_dish(menu)
+
+
+def check_duplicate(menu, dish_name):
+    for dish_category in menu.keys():
+        for dish in menu[dish_category]:
+            if dish["name"] == dish_name:
+                return True
+    return False
