b3b3258f8332a1c03a7ebf0469a9ef5f4b0675a4
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-07-31 17:23:50 +0300

    Commit message >>>>> The Update and Search Dish is all done <<<<<

--- SearchDish.py
+++ SearchDish.py
@@ -1,15 +1,14 @@
 def search_dish(menu):
     dish_name = input("Enter the dish name\nDish : ")
 
-    for category, dishes in menu.items():
-        for dish in dishes:
+    for category in menu.keys():
+        for dish in menu[category]:
             if dish["name"] == dish_name:
                 return dish
     return None
 
 
 def print_dish(menu):
-
     dish = search_dish(menu)
 
     if dish is None:
